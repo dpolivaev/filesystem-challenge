389df86aca3763f3e78d014b159506fd69d307e9
Dimitry Polivaev <dpolivaev@gmx.de>
2018-11-30 20:49:28 +0100

    format

--- src/test/java/org/dpolivaev/katas/filesystem/domain/internal/memory/ReservedBitsTest.java
+++ src/test/java/org/dpolivaev/katas/filesystem/domain/internal/memory/ReservedBitsTest.java
@@ -48,8 +48,8 @@ public class ReservedBitsTest {
                 24L, 25L, 26L, 27L, 28L, 29L, 30L, 31L,
                 32L, 33L, 34L, 35L, 36L, 37L, 38L, 39L,
                 16L, 17L, 18L, 19L, 20L, 21L, 22L, 23L,
-                0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L,
-                10L, 11L, 12L, 13L, 14L, 15L
+                0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L,
+                8L, 9L, 10L, 11L, 12L, 13L, 14L, 15L
         );
     }
 }
