774ee9e4b5c788ce656541a8ce81cc911ca876a6
Dimitry Polivaev <dpolivaev@gmx.de>
2018-12-07 17:52:44 +0100

    add demo application TestApp

---
+++ src/test/java/org/dpolivaev/katas/filesystem/TestApp.java
@@ -0,0 +1,27 @@
+package org.dpolivaev.katas.filesystem;
+
+
+public class TestApp {
+    public static void main(final String... args) {
+        final String fileName = args.length > 0 ? args[0] : "hello.fs";
+        final java.io.File file = new java.io.File(fileName);
+        if (!file.exists())
+            createFileSystem(file.getPath());
+        else
+            useFileSystem(file.getPath());
+    }
+
+    private static void createFileSystem(final String path) {
+        try (final FileSystem fileSystem = FileSystem.create(path, 3072)) {
+            fileSystem.root().createFile("hello").write("Hello world");
+        }
+        System.out.println("Filesystem created in file " + path);
+    }
+
+    private static void useFileSystem(final String path) {
+        try (final FileSystem fileSystem = FileSystem.open(path, 3072)) {
+            System.out.println(fileSystem.root().file("hello").get().readString());
+        }
+    }
+
+}
